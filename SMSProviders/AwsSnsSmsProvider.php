ae8294055fea31f77b0625101c0be12b27f1a57d
Catherine Doherty <catherine@gcdtech.com>
2017-10-05 17:29:45 +0100

    Aws sns sms provider

---
+++ SMSProviders/AwsSnsSmsProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Rhubarb\AwsSnsSmsProvider\SMSProviders;
+
+use Aws\Sns\SnsClient;
+use Gcdtech\Aws\Settings\AwsSettings;
+use Rhubarb\Crown\Sendables\Sendable;
+use Rhubarb\Crown\Sendables\SMS\SMSProvider;
+
+class AwsSnsSmsProvider extends SMSProvider
+{
+    /** @var SnsClient */
+    private $client;
+
+    public function getClient()
+    {
+        if (!isset($this->client)) {
+            $awsSettings = AwsSettings::singleton();
+            $settings = $awsSettings->getClientSettings();
+
+            $this->client = new SnsClient($settings);
+        }
+        return $this->client;
+    }
+
+    /**
+     * Sends the sendable.
+     *
+     * Implemented by the concrete provider type.
+     *
+     * @param Sendable $sendable
+     * @return mixed
+     */
+    public function send(Sendable $sendable)
+    {
+        foreach($sendable->getRecipients() as $recipient) {
+            $this->client->publish(['Message' => $sendable->getText(), 'PhoneNumber' => $recipient]);
+        }
+    }
+}
